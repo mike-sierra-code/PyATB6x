85195018f93a552df3fb2e8508865c3e2ff70673
mike-sierra-code <mike.sierra6e@gmail.com>
2025-10-20 16:31:25 +0530

    New Assignment's Added

---
+++ Src_Minhaz/Daily_Hands_On_Assignemnts/Day - 2 Assignments/Task 1 - Multiplication_&_Sum_Of_Two_Numbers.py
@@ -0,0 +1,25 @@
+# Given Numbers
+# Number 1 = 20
+# Number 2 = 30
+# Number 3 = 40
+# Number 4 = 80
+
+Num1 = 20
+Num2 = 30
+Num3 = 40
+Num4 = 80
+X = Num1*Num2
+print( "The Final Value is:" ,X)
+Y = Num3+Num4
+print( "The Final Value is:" ,Y)
+
+Num5 = int(input("Enter a number :"))
+Num6 = int(input("Enter a number :"))
+Z = Num5*Num6
+print( "The Final Value is:" ,Z)
+
+Num7 = int(input("Enter a number :"))
+Num8 = int(input("Enter a number :"))
+P= Num7+Num8
+print( "The Final Value is:" ,P)
+
